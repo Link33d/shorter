d2072e484ac6096fc151854b659c3ec8a67ce469
Link33d <boneitest@gmail.com>
2024-11-16 16:23:46 -0300

    first API route

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	server := gin.Default()
 
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.Run(":8080")
 }
