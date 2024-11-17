c4bf06560ac0294bac629645c3befee3c0852549
Link33d <boneitest@gmail.com>
2024-11-17 01:04:55 -0300

    feat: update handler structure

--- router/router.go
+++ router/router.go
@@ -1,37 +1,18 @@
 package router
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/link33d/shorter/handler"
 )
 
 func Initialize(server *gin.Engine) {
 
 	// Index page router
-	server.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	server.GET("/", handler.IndexPage)
 
 	// Create link router
-	server.POST("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"status": "ok",
-		})
-	})
+	server.POST("/", handler.CreateShortUrl)
 
 	// Redirect router
-	server.GET("/:code", func(ctx *gin.Context) {
-
-		code := ctx.Param("code")
-
-		fmt.Println(code)
-
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": code,
-		})
-	})
+	server.GET("/:code", handler.RedirectShortUrl)
 }
